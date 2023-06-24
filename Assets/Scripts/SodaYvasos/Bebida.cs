a914a6991a44f16f0428afa80b9968b2cc9cce53
DeionGit <Deionyester.t@gmail.com>
2023-06-24 18:13:11 +0200

    Project

---
+++ Assets/Scripts/SodaYvasos/Bebida.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bebida : MonoBehaviour
+{
+    [SerializeField] KeyCode testingKey;
+
+
+    [SerializeField] float rayToPos = 0.1f;
+    [SerializeField] LayerMask LayersToIgnore;
+
+    bool colocadoEnBandeja = false;
+    Collider vasoCollider;
+    Collider bandejaCollider;
+
+    [Header("Liquido del vaso")]
+    public Liquid InsideLiquid;
+    public GameObject LiquidObject;
+    void Start()
+    {
+        vasoCollider = GetComponent<Collider>();
+        LiquidObject = transform.GetChild(0).gameObject;
+        InsideLiquid = GetComponentInChildren<Liquid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(testingKey))
+        {
+            ColocarEnbandeja();
+        }
+    }
+
+
+    public void ColocarEnbandeja()
+    {
+        if (HayBandeja())
+        {
+            Physics.Raycast(transform.position, -transform.up, out RaycastHit hit);
+            bandejaCollider = hit.collider;
+            Physics.IgnoreCollision(vasoCollider, hit.collider, true);
+            colocadoEnBandeja = true;
+            transform.position = hit.point;
+            transform.parent = hit.transform;
+        }
+    }
+    public void QuitarDeBandeja()
+    {
+        Physics.IgnoreCollision(vasoCollider, bandejaCollider, false);
+        colocadoEnBandeja = false;
+    }
+    bool HayBandeja()
+    {
+        if (Physics.Raycast(transform.position, -transform.up,rayToPos, LayersToIgnore))
+        {
+            return true;
+        }
+        else return false;
+ 
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 to = new Vector3(0, rayToPos, 0);
+
+    }
+
+    
+
+}
+public enum TipoBebida
+{
+    Cerveza,
+    SodaAzul,
+    SodaVerde,
+    SodaRoja,
+    SodaMorada
+}
